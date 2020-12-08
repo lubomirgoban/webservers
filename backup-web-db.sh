2b0f2e1065d68440751a325b101524b092505b7e
Lubomir Goban <49732876+lubomirgoban@users.noreply.github.com>
2020-12-08 13:08:52 +0100

    Update backup-web-db.sh

--- backup-web-db.sh
+++ backup-web-db.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Change domain per domain
-DOMAIN=sarisskagaleria_sk
+USERNAME=
+DOMAIN=
 #EXCLUDE=folder-name
 ###
 TIME=`date +\%F-%HH-%mM-%SS`                # This Command will read the date.
 FILENAME=$DOMAIN-$TIME.tar.gz               # The filename including the date.
 DBNAME=$DOMAIN-DB-$TIME.zip                 # The DB name including the date.
-SRCDIR=/home/spgo/webapps/$DOMAIN           # Source backup folder.
-DESDIR=/home/spgo/backups/$DOMAIN/WEB       # Destination of backup file.
-DESDIRDB=/home/spgo/backups/$DOMAIN/DB      # Destination of DB backup file.
+SRCDIR=/home/$USERNAME/webapps/$DOMAIN           # Source backup folder.
+DESDIR=/home/$USERNAME/backups/$DOMAIN/WEB       # Destination of backup file.
+DESDIRDB=/home/$USERNAME/backups/$DOMAIN/DB      # Destination of DB backup file.
 
 if [ -d "$DESDIR" ]; then
   ### Take action if $DIR exists ###
@@ -17,7 +18,7 @@ if [ -d "$DESDIR" ]; then
   rclone copy $DESDIR/$FILENAME gdrive-backup:$DOMAIN
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  mkdir -p /home/spgo/backups/$DOMAIN/WEB
+  mkdir -p /home/$USERNAME/backups/$DOMAIN/WEB
   #tar -cpzf $DESDIR/$FILENAME --exclude "$EXCLUDE" $SRCDIR
   tar -cpzf $DESDIR/$FILENAME $SRCDIR
   rclone copy $DESDIR/$FILENAME gdrive-backup:$DOMAIN
@@ -36,7 +37,7 @@ if [ -d "$DESDIRDB" ]; then
   rclone copy $DESDIRDB/$DBNAME gdrive-backup:$DOMAIN
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  mkdir -p /home/spgo/backups/$DOMAIN/DB
+  mkdir -p /home/$USERNAME/backups/$DOMAIN/DB
   mv *.sql $DESDIRDB
   # Change directory to zip sql file
   cd $DESDIRDB
